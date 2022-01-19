2ff08d7e8e114c218cea56c63084c89310d7932f
Silvina Staniszewski <silvina.stani@gmail.com>
2022-01-19 20:06:22 -0300

    second commit

---
+++ src/data.js
@@ -0,0 +1,28 @@
+
+export const bootcamp = [
+    {
+      id: 1,
+      title: "TodoList: Vacation",
+      img:
+        "https://user-images.githubusercontent.com/90510746/148145113-25ef5a46-17d7-4c5f-b9e0-07716ffc0b02.png",
+    },
+    {
+      id: 2,
+      title: "Chronometer",
+      img:
+        "https://user-images.githubusercontent.com/90510746/148205845-1f650bfd-15ad-433c-839d-962786b50e83.png",
+    },
+    
+    {
+      id: 3,
+      title: "Todo College",
+      img:
+        "https://user-images.githubusercontent.com/90510746/150045096-6a28da31-440b-43a6-9cde-8498918c81b9.jpg",
+    },
+    {
+      id: 4,
+      title: "Todo Groceries",
+      img:
+        "https://user-images.githubusercontent.com/90510746/150045140-debb6bb9-aa96-4f91-8595-11f5f6dc441e.jpg",
+    },
+  ];
