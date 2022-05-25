f8277795eb91bd4f59fe039f0f972e7f7f2bb6a9
Silvina Staniszewski <silvina.stani@gmail.com>
2022-05-24 23:17:20 -0300

    resume

--- src/components/topbar/TopBar.jsx
+++ src/components/topbar/TopBar.jsx
@@ -11,18 +11,22 @@ export default function TopBar({ menuOpen, setMenuOpen}) {
                     <div className="itemContainer">
                     <WhatsApp className="icon"/>
                     <a href="https://api.whatsapp.com/send/?phone=5491163632288&text&app_absent=0">+549-011-1563632288</a>
+                    {/*<img src="/assets/contact_me.png" alt="" height={50} width={50}/>*/}
                     </div>
                     <div className="itemContainer">
                         <Mail className="icon" />
                         <a href = "mailto:silvina@gmail.com?subject = Feedback&body = Message">silvina.stani@gmail.com</a>
+                        {/*<img src="/assets/mail.png" alt="" height={50} width={50}/>*/}
                     </div>
                     <div className="itemContainer">
                         <GitHub className="icon" />
                        <a href="https://github.com/SilviStani" className="icon">SilviStani</a>
+                        {/*<img src="/assets/github_st.png" alt="" height={50} width={50}/>*/}
                        </div>
                     <div className="itemContainer">
                         <LinkedIn className="icon" />
                         <a href="https://www.linkedin.com/in/silvina-staniszewski-18493276/" className="icon">Silvina Staniszewski</a>
+                       {/*} <img src="/assets/trabajo.png" alt="" height={50} width={50}/>*/}
                     </div>
                 </div>
                 <div className="right">
