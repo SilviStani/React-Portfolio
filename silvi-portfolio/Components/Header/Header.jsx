f8f7491bf1fba62f4373e4984a5a6b7575af8d7a
Silvina Staniszewski <silvina.stani@gmail.com>
2024-02-13 08:36:13 -0300

    adding new files and folders

---
+++ silvi-portfolio/Components/Header/Header.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import styles from "../../Styles/Header.module.css";
+import { data } from "../Header/data.js";
+import Link from "next/link";
+
+const Header = () => {
+  return (
+    <header className={styles.HeaderContainer}>
+      <ul className={styles.rowHead}>
+        {data.map((e) => (
+          <Link href={e.url} target="_blank" className={styles.links}>
+            <li key={e.id} className={styles.li}>{e.title}</li>
+          </Link>
+        ))}
+      </ul>
+    </header>
+  );
+};
+
+export default Header;
---
+++ silvi-portfolio/Components/Header/data.js
@@ -0,0 +1,22 @@
+export const data = [
+    {
+        id: 1,
+        title: "Github",
+        url: "https://github.com/SilviStani/",
+    },
+    {
+        id: 2,
+        title: "Whatsapp",
+        url: "https://wa.me/5491163632288?text=Hola!%20Me%20interesa%20información%20sobre%20las%20páginas%20Web.%0AGracias!",
+    },
+    {
+        id: 3,
+        title: "Trabajos",
+        url: "/works",
+    },
+    {
+        id: 1,
+        title: "Linkedin",
+        url: "https://www.linkedin.com/in/silvina-staniszewski-18493276/",
+    },
+] 
